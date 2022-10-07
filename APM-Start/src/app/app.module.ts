2f312b06e509dbe71f2492862dd23db4323a8bae
FlagshipKagero <52209656+FlagshipKagero@users.noreply.github.com>
2022-10-07 16:35:00 +0800

    Update app.module.ts

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,15 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
+import { ProductModule } from "./products/product.module";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { PageNotFoundComponent } from "./home/pagenotfound.component";
+import { WelcomeComponent } from "./home/welcome.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, WelcomeComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    //feature/shared modules should be above because
+    //they may contain routing, which cannot be below the wildcard route
+    ProductModule,
+    //wildcard route should always be at the bottom
+    RouterModule.forRoot([
+      { path: "welcome", component: WelcomeComponent },
+      { path: "", component: WelcomeComponent, pathMatch: "full" },
+      { path: "**", component: PageNotFoundComponent },
+    ]),
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
